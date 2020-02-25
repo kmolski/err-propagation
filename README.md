767cd669bf0bd141b32fcd02bd4503c5bfb6e52b
kmolski <starfighter413@gmail.com>
2020-02-25 21:07:29 +0100

    Added a way to convert formulas to infix notation

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # err-propagation
 
-A small program that calculates complex uncertainty values for variables in a formula.
+A collection of functions for differentiation and propagation of error
+on formulas with arbitrary variables.
 
 So far, it has only been tested with GNU Guile Scheme 2.2.6.
 
@@ -10,21 +11,37 @@ Credits:
 The derivation procedure used in this program is adapted from the book
 "Structure and Interpretation of Computer Programs, Second Edition" by H. Abelson, G.J. Sussman and J. Sussman.
 
+Usage:
+------
+
+To start the Guile REPL with the functions loaded, run:
+
+```sh
+guile -l err-propagation.scm
+```
+
 Examples:
 --------
 
 Propagation of error for formula `2 * f * dx-avg`:
 
-```
+```scheme
 scheme@(guile-user)> (err-propagate '(* 2 (* f dx-avg)) '((dx-avg 0.1) (f 0.01)))
 $1 = (sqrt (+ (^ (* (* 2 dx-avg) 0.01) 2) (^ (* (* 2 f) 0.1) 2)))
 ```
 
 The result of `err-propagate` can also be evaluated:
 
-```
+```scheme
 scheme@(guile-user)> (define dx-avg 0.072)
 scheme@(guile-user)> (define f 1200)
 scheme@(guile-user)> (eval $1 (interaction-environment))
 $2 = 240.00000000432
+```
+
+or turned into an Excel-compatible infix formula:
+
+``` scheme
+scheme@(guile-user)> (expr-to-infix-str $1)
+$3 = "SQRT(((2 * dx-avg) * 0.01) ^ 2 + ((2 * f) * 0.1) ^ 2)"
 ```
--- err-propagation.scm
+++ err-propagation.scm
@@ -1,4 +1,5 @@
-(use-modules (ice-9 match))
+(use-modules (ice-9 format)
+             (ice-9 match))
 
 (define (same-variable? a b)
   (and (symbol? b) (eq? a b)))
@@ -59,32 +60,32 @@
         (else (list 'cos x))))
 
 (define (derive expr var)
-  ;; Calculate derivative of expr with respect to var.
+  ;; Make a derivative of expr with respect to var.
   (match expr
-         ((? number? _) 0)
-         ((? symbol? _) (if (same-variable? expr var) 1 0))
+    ((? number? _) 0)
+    ((? symbol? _) (if (same-variable? expr var) 1 0))
 
-         (('+ a b) (+! (derive a var)
-                       (derive b var)))
-         (('- a b) (-! (derive a var)
-                       (derive b var)))
+    (('+ a b) (+! (derive a var)
+                  (derive b var)))
+    (('- a b) (-! (derive a var)
+                  (derive b var)))
 
-         (('* a b) (+! (*! a (derive b var))
-                       (*! (derive a var) b)))
-         (('/ a b) (/! (-! (*! (derive a var) b)
-                           (*! a (derive b var)))
-                       (^! b 2)))
+    (('* a b) (+! (*! a (derive b var))
+                  (*! (derive a var) b)))
+    (('/ a b) (/! (-! (*! (derive a var) b)
+                      (*! a (derive b var)))
+                  (^! b 2)))
 
-         (('exp x) (*! (derive x var) expr))
-         (('log x) (/! (derive x var) x))
-         (('^ a b) (*! (+! (*! (derive a var) (/! b a))
-                           (*! (derive b var) (log! a)))
-                       expr))
+    (('exp x) (*! (derive x var) expr))
+    (('log x) (/! (derive x var) x))
+    (('^ a b) (*! (+! (*! (derive a var) (/! b a))
+                      (*! (derive b var) (log! a)))
+                  expr))
 
-         (('sin x) (*! (derive x var) (cos! x)))
-         (('cos x) (*! (derive x var) (-! 0 (sin! x))))
+    (('sin x) (*! (derive x var) (cos! x)))
+    (('cos x) (*! (derive x var) (-! 0 (sin! x))))
 
-         (_ (error "unknown expression type" expr))))
+    (_ (error "unknown expression type" expr))))
 
 (define (err-propagate expr vars)
   ;; Make an error propagation formula for the given expression and variables.
@@ -102,3 +103,41 @@
           (square-sum (cons (^! (*! expr-by-var var-error) 2) elems) (cdr varl)))))
   ;; The result is the square root of the sum.
   (list 'sqrt (square-sum '() vars)))
+
+(define (expr-to-infix-str expr)
+  ;; Display an expression in infix notation.
+  (define default-name-map '((exp . "EXP") (log . "LOG") (sin . "SIN") (cos . "COS") (sqrt . "SQRT")))
+  (define op-precedence-map '((+ . 1) (- . 1) (* . 2) (/ . 2) (^ . 3)))
+
+  (define (exact-fract? x) (and (number? x) (not (integer? x)) (exact? x)))
+
+  (define (wrap-in-parens x) (format #f "(~a)" x))
+
+  (define (subexpr-to-str part order)
+    ;; Convert subexpression into a string
+    (let ((part-str (expr-to-infix-str part)))
+      (match part
+        ((or (? number? _) (? symbol? _)
+             ((or 'exp 'log 'sin 'cos 'sqrt) _)) part-str)
+        ;; Put parens around part if it has a binary operator
+        ;; and the previous operator has higher precedence
+        ((op _ _) (if (<= (assq-ref op-precedence-map op) order)
+                      (wrap-in-parens part-str)
+                      part-str))
+        (_ (error "unknown expression type" part)))))
+
+  (match expr
+    ((? exact-fract? x) (wrap-in-parens x)) ; Fractions
+    ((? number? x) (number->string x))      ; Floats and integers
+    ((? symbol? x) (symbol->string x))      ; Variable names
+
+    (((and (or 'exp 'log 'sin 'cos 'sqrt) op) x)  ; Single-argument functions
+     (format #f "~a(~a)" (assq-ref default-name-map op) (expr-to-infix-str x)))
+
+    (((and (or '+ '- '* '/ '^) op) a b)
+     (let* ((op-order (assq-ref op-precedence-map op))
+            (a-str (subexpr-to-str a op-order))
+            (b-str (subexpr-to-str b op-order)))
+       (format #f "~a ~a ~a" a-str op b-str))) ; Two-argument infix operators
+
+    (_ (error "unknown expression type" expr))))
